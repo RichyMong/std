d049cba35041bd908f780b75e984ef29b40ea202
Richy Mong <RichyMong@gmail.com>
2015-01-22 15:05:30 +0800

    add RAII thread

--- cpp/misc/thread.cpp
+++ cpp/misc/thread.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 #include <thread>
+#include <chrono>
+
+namespace {
+
+struct guarded_thread : public std::thread {
+    using std::thread::thread;
+    ~guarded_thread() { if (joinable()) join(); }
+};
+
+void tick(int n)
+{
+    for (int i = 0; i != n; ++i) {
+        std::this_thread::sleep_for(std::chrono::seconds {1});
+        std::cout << __FILE__ << ":" << __LINE__ << " --- " << i + 1 << std::endl;
+    }
+}
+
+}
 
 int main()
 {
-    std::cout << "Number of cores: " << std::thread::hardware_concurrency() << std::endl;
+    std::cout << "Number of cores: " << std::thread::hardware_concurrency()
+              << std::endl;
+
+    guarded_thread gt { tick, 10 };
 
     return 0;
 }
