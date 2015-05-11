701742b8735776f38fab532d52496247f0ce2934
RichyMong <RichyMong@gmail.com>
2015-05-11 22:23:48 +0800

    add some sed tips

--- python/misc/connect.py
+++ python/misc/connect.py
@@ -4,9 +4,9 @@ import select
 
 connections = []
 
-for i in range(5, 800):
+for i in range(1, 200):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect_ex(('127.0.0.1', 12345))
     connections.append(s)
 
-time.sleep(20)
+time.sleep(10)
