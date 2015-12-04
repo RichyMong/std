605fd1dc1422e2641cdcff4827f9d967900a3352
RichyMong <RichyMong@gmail.com>
2015-12-04 20:12:26 +0800

    update

---
+++ web/www/php/register_result.php
@@ -0,0 +1,8 @@
+<html>
+    <body/>
+        <?php
+            echo 'Your username: ' . $_POST['username'] . '</br>';
+            echo 'Your email: ' . $_POST['email'] . '</br>';
+        ?>
+    </body>
+</html>
