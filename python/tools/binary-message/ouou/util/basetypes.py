3960f1c40ee9aa5e85ecf97b28aa39729b0a49d8
RichyMong <RichyMong@gmail.com>
2016-04-10 19:16:20 +0800

    split types into base types like int, str and comptypes like Array, Vector

---
+++ python/tools/binary-message/ouou/util/basetypes.py
@@ -0,0 +1,115 @@
+import struct
+
+__all__ = ['Char', 'Byte', 'Short', 'UShort', 'Int', 'UInt', 'LargeInt',
+           'Long', 'ULong' ]
+
+BYTE_ORDER = '<'
+
+def tobytes(obj):
+    if isinstance(obj, str):
+        return struct.pack(obj.pack_fmt, obj.encode())
+    else:
+        return struct.pack(BYTE_ORDER + obj.pack_fmt, obj)
+
+def size(obj):
+    '''
+    Though the sizes of most of the types are fixed, we use an instance
+    method so that we provide the same interfaces as the types which
+    are determined by instances, such as String, Vector.
+    '''
+    return type(obj).type_size
+
+
+class LegacyTypeMeta(type):
+    def __new__(meta, classname, supers, classdict):
+        cls = type.__new__(meta, classname, supers, classdict)
+        cls.tobytes = tobytes
+        cls.type_size = struct.calcsize(cls.pack_fmt)
+        cls.size = size
+
+        return cls
+
+
+    def frombytes(cls, buf, offset = 0):
+        value = struct.unpack_from(cls.pack_fmt, buf, offset)[0]
+        if issubclass(cls, str):
+            return cls(value.decode())
+        else:
+            return cls(value)
+
+
+    def fromstream(cls, reader, **kwargs):
+        buf = reader.read_bytes(cls.type_size)
+        return cls.frombytes(buf)
+
+
+class Char(str, metaclass = LegacyTypeMeta):
+    pack_fmt = 'c'
+
+    def __init__(self, arg):
+        if len(arg) > 1:
+            raise ValueError('Char contains more than one character')
+        super(str, Char).__init__(arg)
+
+class Byte(int, metaclass = LegacyTypeMeta):
+    pack_fmt = 'B'
+
+class Short(int, metaclass = LegacyTypeMeta):
+    pack_fmt = 'h'
+
+class UShort(int, metaclass = LegacyTypeMeta):
+    pack_fmt = 'H'
+
+class Int(int, metaclass = LegacyTypeMeta):
+    pack_fmt = 'i'
+
+class UInt(int, metaclass = LegacyTypeMeta):
+    pack_fmt = 'I'
+
+class Long(int, metaclass = LegacyTypeMeta):
+    pack_fmt = 'l'
+
+class ULong(int, metaclass = LegacyTypeMeta):
+    pack_fmt = 'L'
+
+class LargeInt(int, metaclass = LegacyTypeMeta):
+    pack_fmt = 'i'
+
+    def __init__(self, value):
+        self.value = value
+
+    @staticmethod
+    def frombytes(buf, offset = 0):
+        value = Int.frombytes(buf, offset)
+        negative = False
+        if value < 0:
+            negative = True
+            value = -value
+        tmp = value >> 29
+        if tmp:
+            value &= 0x1fffffff
+            value <<= tmp * 4
+
+        return LargeInt(value) if not negative else LargeInt(-value)
+
+
+    def __len__(self):
+        return 4
+
+    def tobytes(self):
+        return Int(self.value).tobytes()
+
+
+if __name__ == '__main__':
+    sh = Short(0x1234)
+    i = Int(0x4578ffff)
+    c = Char('h')
+    print(type(c))
+    print(sh, i, c)
+    b = sh.tobytes() + i.tobytes() + c.tobytes()
+    print(len(b))
+    sh = Short.frombytes(b, 0)
+    i = Int.frombytes(b, 2)
+    c = Char.frombytes(b, 6)
+    print(sh, i, c)
+    print(sh.size())
---
+++ python/tools/binary-message/ouou/util/comptypes.py
@@ -0,0 +1,107 @@
+import struct
+from functools import reduce
+from .basetypes import *
+from .functions import *
+
+__all__ = [ 'TypeList', 'Array', 'Vector', 'ByteVector', 'String', 'GBKString' ]
+
+def EncodedString(encoding):
+    class WrapperString(str):
+        @classmethod
+        def fromstream(cls, reader):
+            n = reader.read_short()
+            s = reader.read_bytes(n)
+            return cls(s.decode(encoding = encoding))
+
+        def size(self):
+            '''
+            Either from or to bytes, the size is decided by encoding. Since we
+            inherit str, we are not able to add a length field to save the size.
+            '''
+            return 2 + len(self.encode(encoding = encoding))
+
+        def tobytes(self):
+            s = self.encode(encoding = encoding)
+            b = Short(len(s)).tobytes()
+            return b + struct.pack('{}s'.format(len(s)), s)
+
+    return WrapperString
+
+String = EncodedString('utf-8')
+GBKString = EncodedString('gbk')
+
+def TypeList(elem_cls):
+    class WrapperList(list):
+        def append(self, obj):
+            if not isinstance(obj, elem_cls):
+                obj = elem_cls(obj)
+            super().append(obj)
+
+        def tobytes(self):
+            b = b''
+            for elem in self:
+                if isinstance(elem, elem_cls):
+                    b += elem.tobytes()
+                else:
+                    b += elem_cls(elem).tobytes()
+            return b
+
+        def size(self):
+            if hasattr(elem_cls, 'type_size'):
+                return elem_cls.type_size * len(self)
+            else:
+                return sum(x.size() for x in self)
+
+        def __repr__(self):
+            if issubclass(elem_cls, int):
+                return '['  + to_range_str(self) + '] ({})'.format(len(self))
+            elif issubclass(elem_cls, str):
+                return '[ ' + ' '.join(x for x in self) + '] ({})'.format(len(self))
+            else:
+                r = '共 {} 个'.format(len(self))
+                for x in self:
+                    r += '\n\t' + 80 * '-' + '{}'.format(x)
+                return r
+
+    return WrapperList
+
+def Array(array_size, elem_cls):
+    class Wrapped(TypeList(elem_cls)):
+        @classmethod
+        def fromstream(cls, reader):
+            return cls(reader.read_times(elem_cls, array_size))
+
+    return Wrapped
+
+def Vector(size_cls, elem_cls):
+    class Wrapped(TypeList(elem_cls)):
+        @classmethod
+        def fromstream(cls, reader):
+            size = reader.read_type(size_cls)
+            return cls(reader.read_times(elem_cls, size))
+
+        def size(self):
+            return size_cls.type_size + super().size()
+
+        def tobytes(self):
+            return size_cls(len(self)).tobytes() + super().tobytes()
+
+    return Wrapped
+
+ByteVector = Vector(Byte, Byte)
+
+if __name__ == '__main__':
+    ay = Vector(Byte, Int)()
+    ay.append(1)
+    ay.append(2)
+    ay.append(1)
+    ay.append(1)
+    print(len(ay), ay)
+    b = ay.tobytes()
+    print(len(b))
+    ay = Vector(Byte, Int).frombytes(b)
+    print(len(ay), ay)
+    s = String('hello')
+    b = s.tobytes()
+    print(len(b), b)
+    print(String.frombytes(b))
