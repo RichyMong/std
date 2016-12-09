8a0df479032ac753152b2928cf145fe67a598c14
RichyMong <RichyMong@gmail.com>
2016-12-09 23:18:17 +0800

    add sso test

--- cpp/misc/string.cpp
+++ cpp/misc/string.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
+using namespace std;
+
+    static union {
+        int x;
+        long y;
+    };
+struct Test {
+
+    Test() { y = 100; }
+};
 
 int main() {
     std::string s = "abc";
     std::cout << "size: " << s.size() << std::endl;
+    Test x;
+    std::cout << y << '\n';
+
+
+    cout << "sizeof(string)=" << sizeof(string) << '\n';
+    cout << "sizeof(string(\"hi\"))=" << sizeof(string("hi")) << '\n';
+    std::string a[10];
+    std::cout << "sizeof(string[10])=" << sizeof(a) << '\n';
+    std::vector<std::string> v(10);
+    cout << "sizeof(vector)=" << sizeof(vector<string>) << '\n';
+    std::cout << "sizeof(vector<string>(10))="  <<sizeof(v) + sizeof(std::string) * v.capacity() << "\n";
+
 }
